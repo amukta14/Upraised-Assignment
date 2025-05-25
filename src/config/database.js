a450a5f35a447189974d668475c1bbf1f9882c80
Amukta Yajamanyam <amukta@Amuktas-MacBook-Air.local>
2025-05-25 22:43:03 +0530

    Update database configuration to use internal connection URL

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize('postgres://imf_gadget_db_91u3_user:MeXnGhLuXHrQ7XIDoO4gtbaJNR15ikWo@dpg-d0pkr0re5dus73dumn10-a:5432/imf_gadget_db_91u3', {
+const sequelize = new Sequelize('postgresql://imf_gadget_db_91u3_user:MeXnGhLuXHrQ7XIDoO4gtbaJNR15ikWo@dpg-d0pkr0re5dus73dumn10-a/imf_gadget_db_91u3', {
   dialect: 'postgres',
   dialectOptions: {
     ssl: {
